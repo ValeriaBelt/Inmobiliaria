22d37dcf047fd0981519da7467931e915850ba38
ValeriaBelt <144816556+ValeriaBelt@users.noreply.github.com>
2025-04-14 22:12:09 -0500

    Add files via upload

---
+++ ApplicationDbContext.cs
@@ -0,0 +1,31 @@
+﻿using Apartamentos.Modelo;
+using Microsoft.EntityFrameworkCore;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    { }
+
+
+
+    public DbSet<Apto> apartamento { get; set; }
+    public DbSet<Inquilino> inquilino { get; set; }
+    public DbSet<Personal> personal { get; set; }
+    public DbSet<Contrato> contrato { get; set; }
+    public DbSet<Mantenimiento> mantenimiento { get; set; }
+    public DbSet<Pago> pago { get; set; }
+    public DbSet<Incidencia> incidencia { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        // Configuraciones personalizadas (si es necesario)
+        modelBuilder.Entity<Contrato>()
+            .HasOne(c => c.apartamento)
+            .WithMany()
+            .HasForeignKey(c => c.idapartamento);
+    }
+
+}
